e3fecbde264bc6c955839325b57d4b6c53ea073f
sainiggn <sainiggn.92@gmail.com>
2021-12-21 16:59:24 +0530

    Update settings.py

--- newblog/settings.py
+++ newblog/settings.py
@@ -43,11 +43,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'embed_video'
+    'embed_video',
+    'whitenoise'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
